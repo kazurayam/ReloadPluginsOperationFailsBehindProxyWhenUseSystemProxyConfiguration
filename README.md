63558c99784e87132d05585ed05226d9b54dbd7c
kazurayam <kazuaki.matsuhashi@quick.jp>
2019-09-19 17:00:28 +0900

    updates

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# Desired Capabilities defined by the W3C WebDriver specification
+
+https://w3c.github.io/webdriver/#protocol
---
+++ Scripts/TC1_getIntranetHost/Script1568872308518.groovy
@@ -0,0 +1,4 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+WebUI.openBrowser('')
+WebUI.navigateToUrl('http://172.24.172.3/default.htm')
